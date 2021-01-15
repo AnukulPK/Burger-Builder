f3d578a6b3abf78f7095529fd7fca6a1206f6acf
Anukul <39963783+AnukulPK@users.noreply.github.com>
2021-01-15 15:24:13 +0530

    nav bar update

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import './Layout.css';
+import SideDrawer from  '../Navigation/SideDrawer/SideDrawer';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const Layout = (props)=>{
     return(
     <>
     <Toolbar/>
+    <SideDrawer/>
     <main className="content">
         {props.children}
     </main>
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import './Toolbar.css';
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props)=>(
     <header className="Toolbar">
         <div>MENU</div>
         <Logo/>
-        <nav>
-            ...
-        </nav>
+        <NavigationItems/>
     </header>
 )
 
